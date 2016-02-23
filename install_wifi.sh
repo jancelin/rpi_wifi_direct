1d650b488558bc07c726b7743cb24986c7b38957
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2016-02-23 12:19:06 +0100

    Update install_wifi.sh

--- install_wifi.sh
+++ install_wifi.sh
@@ -24,15 +24,15 @@ mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bak
 wget -P /etc/hostapd https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/hostapd.conf
 
 mv /etc/network/interfaces /etc/network/interfaces.bak
-wget -P /etc/network/interfaces https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/interfaces
+wget -P /etc/network https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/interfaces
 
 mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
-wget -P /etc/dhcp/dhcpd.conf https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/dhcpd.conf
+wget -P /etc/dhcp https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/dhcpd.conf
 
 mv /etc/default/hostapd /etc/default/hostapd.bak
-wget -P /etc/default/hostapd https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/hostapd
+wget -P /etc/default https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/hostapd
 
 mv rc.local /etc/rc.local.bak
-wget -P /etc/rc.local https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/rc.local
+wget -P /etc https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/rc.local
 
 
