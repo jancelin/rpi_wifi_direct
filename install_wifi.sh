9a7051760dc71b425f1cfe86d5882b885d36ae9f
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2016-02-23 12:21:49 +0100

    Update install_wifi.sh

--- install_wifi.sh
+++ install_wifi.sh
@@ -10,14 +10,21 @@
 #
 #-----------------------------------------------------------
 sudo apt-get -y update
+
 sudo  apt-get install -y hostapd unzip wget nano isc-dhcp-server
 
 wget http://www.daveconroy.com/wp3/wp-content/uploads/2013/07/hostapd.zip
+
 unzip hostapd.zip
+
 mv /usr/sbin/hostapd /usr/sbin/hostapd.bak
+
 mv hostapd /usr/sbin/hostapd.edimax
+
 ln -sf /usr/sbin/hostapd.edimax /usr/sbin/hostapd
+
 chown root.root /usr/sbin/hostapd
+
 chmod 755 /usr/sbin/hostapd
 
 mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bak
