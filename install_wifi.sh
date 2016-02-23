bc1fdc37a0f832f4cd0e5a2508644465a34dd57d
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2016-02-23 11:11:12 +0100

    Update install_wifi.sh

--- install_wifi.sh
+++ install_wifi.sh
@@ -9,7 +9,7 @@
 #
 #
 #-----------------------------------------------------------
-sudo   apt-get install -y hostapd unzip wget nano isc-dhcp-server
+sudo  apt-get install -y hostapd unzip wget nano isc-dhcp-server
 
  wget http://www.daveconroy.com/wp3/wp-content/uploads/2013/07/hostapd.zip
  unzip hostapd.zip
@@ -19,8 +19,15 @@ sudo   apt-get install -y hostapd unzip wget nano isc-dhcp-server
  chown root.root /usr/sbin/hostapd
  chmod 755 /usr/sbin/hostapd
 
-ADD hostapd.conf /etc/hostapd/hostapd.conf
-ADD interfaces /etc/network/interfaces
-ADD dhcpd.conf /etc/dhcp/dhcpd.conf
-ADD hostapd /etc/default/hostapd
-ADD rc.local /etc/rc.local
+mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bak
+wget -P /etc/hostapd/hostapd.conf https://github.com/jancelin/rpi_wfi_direct_edimax/blob/master/hostapd.conf
+mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bak
+wget -P /etc/network/interfaces https://github.com/jancelin/rpi_wfi_direct_edimax/blob/master/interfaces
+mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
+wget -P /etc/dhcp/dhcpd.conf https://github.com/jancelin/rpi_wfi_direct_edimax/blob/master/dhcpd.conf
+mv /etc/default/hostapd /etc/default/hostapd.bak
+wget -P /etc/default/hostapd https://github.com/jancelin/rpi_wfi_direct_edimax/blob/master/hostapd
+mv rc.local /etc/rc.local
+wget -P /etc/rc.local https://github.com/jancelin/rpi_wfi_direct_edimax/blob/master/rc.local
+
+
