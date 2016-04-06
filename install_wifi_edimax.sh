c7e5b8845ae28859db02f79b417ad5e3ad64a4c6
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2016-04-06 09:43:23 +0200

    Update install_wifi_edimax.sh

--- install_wifi_edimax.sh
+++ install_wifi_edimax.sh
@@ -12,37 +12,37 @@
 # Run:
 #   sh /home/pi/install_wifi.sh
 #-----------------------------------------------------------
-sudo apt-get -y update
+sudo apt-get -y update &&
 
-sudo  apt-get install -y hostapd unzip wget nano isc-dhcp-server
+sudo  apt-get install -y hostapd unzip wget nano isc-dhcp-server &&
 
-wget http://www.daveconroy.com/wp3/wp-content/uploads/2013/07/hostapd.zip
+wget http://www.daveconroy.com/wp3/wp-content/uploads/2013/07/hostapd.zip &&
 
-unzip hostapd.zip
+unzip hostapd.zip &&
 
-mv /usr/sbin/hostapd /usr/sbin/hostapd.bak
+mv /usr/sbin/hostapd /usr/sbin/hostapd.bak &&
 
-mv hostapd /usr/sbin/hostapd.edimax
+mv hostapd /usr/sbin/hostapd.edimax &&
 
-ln -sf /usr/sbin/hostapd.edimax /usr/sbin/hostapd
+ln -sf /usr/sbin/hostapd.edimax /usr/sbin/hostapd &&
 
-chown root.root /usr/sbin/hostapd
+chown root.root /usr/sbin/hostapd &&
 
-chmod 755 /usr/sbin/hostapd
+chmod 755 /usr/sbin/hostapd &&
 
-mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bak
-wget -P /etc/hostapd https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/hostapd.conf
+mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bak &&
+wget -P /etc/hostapd https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/hostapd.conf &&
 
-mv /etc/network/interfaces /etc/network/interfaces.bak
-wget -P /etc/network https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/interfaces
+mv /etc/network/interfaces /etc/network/interfaces.bak &&
+wget -P /etc/network https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/interfaces &&
 
-mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
-wget -P /etc/dhcp https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/dhcpd.conf
+mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak &&
+wget -P /etc/dhcp https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/dhcpd.conf &&
 
-mv /etc/default/hostapd /etc/default/hostapd.bak
-wget -P /etc/default https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/hostapd
+mv /etc/default/hostapd /etc/default/hostapd.bak &&
+wget -P /etc/default https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/hostapd &&
 
-mv /etc/rc.local /etc/rc.local.bak
+mv /etc/rc.local /etc/rc.local.bak &&
 wget -P /etc https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/rc.local
 
 
