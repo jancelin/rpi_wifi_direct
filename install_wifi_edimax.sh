91f49b97ac70779773682586c2099bcfc83f8c85
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2016-04-06 09:47:22 +0200

    Update install_wifi_edimax.sh

--- install_wifi_edimax.sh
+++ install_wifi_edimax.sh
@@ -4,7 +4,7 @@
 # Script d'installation du wifi direct avec dongle wifi edimax sur raspberry pi 
 #
 # copier ce fichier sur le rpi ou télécharger le : 
-#   wget -P /home/pi https://raw.githubusercontent.com/jancelin/rpi_wfi_direct_edimax/master/install_wifi.sh
+#   wget -P /home/pi https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/install_wifi_edimax.sh
 #
 # atttribuer droit execution:
 #   chmod +x /home/pi/install_wifi.sh
