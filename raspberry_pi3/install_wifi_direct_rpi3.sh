f20c72d17d9409441faab2de9639c7c207a3d8ac
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2016-07-27 10:58:15 +0200

    Update install_wifi_direct_rpi3.sh

--- raspberry_pi3/install_wifi_direct_rpi3.sh
+++ raspberry_pi3/install_wifi_direct_rpi3.sh
@@ -4,4 +4,6 @@
 
 
 sudo apt-get install dnsmasq hostapd &&
-wget /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/dhcpcd.conf &&
+wget -P /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/dhcpcd.conf &&
+wget -P /etc/network/interfaces.d https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/wlan0 &&
+
