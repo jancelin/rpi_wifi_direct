cde18583a45432fddba2fa89a570880b2f0bc122
ANCELIN Julien <julien.ancelin@inra.fr>
2016-09-15 11:48:12 +0200

    -P --no-check-certificate

--- raspberry_pi3/install_wifi_direct_rpi3.sh
+++ raspberry_pi3/install_wifi_direct_rpi3.sh
@@ -1,22 +1,22 @@
 #!/bin/sh 
-## wget -P /home/pirate https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/install_wifi_direct_rpi3.sh; chmod +x /home/pirate/install_wifi_direct_rpi3.sh; bash -x /home/pirate/install_wifi_direct_rpi3.sh
+## wget -P --no-check-certificate /home/pirate https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/install_wifi_direct_rpi3.sh; chmod +x /home/pirate/install_wifi_direct_rpi3.sh; bash -x /home/pirate/install_wifi_direct_rpi3.sh
 
 
 sudo apt-get update && 
 sudo apt-get upgrade &&
 sudo apt-get install dnsmasq hostapd &&
-wget -P /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/dhcpcd.conf &&
-wget -P /etc/network/interfaces.d https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/wlan0 &&
+wget -P --no-check-certificate /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/dhcpcd.conf &&
+wget -P --no-check-certificate /etc/network/interfaces.d https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/wlan0 &&
 sudo service dhcpcd restart &&
 sudo ifdown wlan0; sudo ifup wlan0 &&
-wget -P /etc/hostapd https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/hostapd.conf &&
+wget -P --no-check-certificate /etc/hostapd https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/hostapd.conf &&
 #sudo /usr/sbin/hostapd /etc/hostapd/hostapd.conf 
 mv /etc/default/hostapd /etc/default/hostapd.bak &&
-wget -P /etc/default https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/hostapd &&
+wget -P --no-check-certificate /etc/default https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/hostapd &&
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig &&
-wget -P /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/dnsmasq.conf &&
+wget -P --no-check-certificate /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/dnsmasq.conf &&
 sudo mv /etc/sysctl.conf /etc/sysctl.conf.bak &&
-wget -P /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/sysctl.conf &&
+wget -P --no-check-certificate /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/sysctl.conf &&
 sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward" &&
 sleep 5
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
@@ -25,7 +25,7 @@ sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
 sleep 5
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat" &&
 sudo mv /etc/rc.local /etc/rc.local.bak &&
-wget -P /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/rc.local &&
+wget -P --no-check-certificate /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/rc.local &&
 chmod +x  /etc/rc.local &&
 sudo service hostapd start 
 sudo service dnsmasq start
