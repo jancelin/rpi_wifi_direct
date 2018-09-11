77f92ec25450c041310681867f753fa2dfedb155
ElRouqin <rtourte@yahoo.fr>
2018-09-11 10:11:50 +0200

    Update getting USB0 connections for routing

--- raspberry_pi3/install_wifi_direct_rpi3.sh
+++ raspberry_pi3/install_wifi_direct_rpi3.sh
@@ -20,8 +20,11 @@ sudo wget --no-check-certificate -P /etc https://raw.githubusercontent.com/jance
 sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward" &&
 sleep 5
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
+sudo iptables -t nat -A POSTROUTING -o usb0 -j MASQUERADE  
 sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
+sudo iptables -A FORWARD -i usb0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
 sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT 
+sudo iptables -A FORWARD -i wlan0 -o usb0 -j ACCEPT 
 sleep 5
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat" &&
 #sudo mv /etc/rc.local /etc/rc.local.bak &&
